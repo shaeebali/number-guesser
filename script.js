19ad859075b770ce158e3e274b7d828108e180fd
shaeeb <shaeebali@gmail.com>
2023-01-24 09:56:00 +0000

    add game logic

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+//declaration of global variables:
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+// game logic below:
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10)
+}
 
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+  const diffHuman = Math.abs(targetGuess - humanGuess)
+  const diffComputer = Math.abs(targetGuess - computerGuess)
+  return diffHuman <= diffComputer;
+}
+
+const updateScore = winner => {
+  if (winner === 'human') {
+    humanScore ++;
+  } else if (winner === 'computer') {
+    computerScore ++;
+  }
+}
+
+const advanceRound = () => currentRoundNumber ++;
